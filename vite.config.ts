3474be254406a1fcf8fb7545990b3744f891feb4
Marwan <marwanna237@gmail.com>
2026-06-14 15:04:25 +0100

    chore: update project configuration
    Rename project to "dev-roadmaps", clean up unused scripts, update base path, and reset example environment variables.

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/RoadMapDz/',
+    base: './',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
